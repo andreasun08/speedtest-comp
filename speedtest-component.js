d3d6d1f6d45a4ef7c440ea82089aabbebbaefcb1
Cherrie Mae Andrea Sun <cesun@up.edu.ph>
2021-04-30 18:51:50 +0800

    please work

--- speedtest-component.js
+++ speedtest-component.js
@@ -1,11 +1,17 @@
 
 async function getNetworkDownloadSpeed() {
-    const NetworkSpeed = require('network-speed');
-    const testNetworkSpeed = new NetworkSpeed();
-    var baseUrl = 'https://eu.httpbin.org/stream-bytes/500000';
-    var fileSizeInBytes = 500000;
-    var speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
-    return speed;
+
+    try {
+        const NetworkSpeed = require('network-speed');
+        const testNetworkSpeed = new NetworkSpeed();
+        var baseUrl = 'https://eu.httpbin.org/stream-bytes/6000000';
+        var fileSizeInBytes = 6000000;
+        var speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
+        return speed;
+    } catch (e) {
+        return e.message;
+    }
+
 }
 
 async function getNetworkUploadSpeed() {
@@ -20,53 +26,85 @@ async function getNetworkUploadSpeed() {
             'Content-Type': 'application/json',
         },
     };
-    var fileSizeInBytes = 2000000
+    var fileSizeInBytes = 4000000
     var speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
     return speed;
 }
 
+
 async function getPing() {
-    var result = await ping('https://google.com');
-    return result;
+    try {
+        var result = await ping('https://google.com');
+        return result;
+    } catch (error) {
+        return error.message;
+    }
+
 }
 
 async function getServiceProvider() {
-    const http = require('http');
-    return http.get('http://ip-api.com/json/', (res) => {
-        const { statusCode } = res;
-        const contentType = res.headers['content-type'];
+    return new Promise(function (resolve, reject) {
+        const http = require('http');
+        var startTime = new Date().getTime();
+        http.get('http://ip-api.com/json/', (res) => {
+            const { statusCode } = res;
+            const contentType = res.headers['content-type'];
+            var endTime = new Date().getTime();
 
-        let error;
-        // Any 2xx status code signals a successful response but
-        // here we're only checking for 200.
-        if (statusCode !== 200) {
-            error = new Error('Request Failed.\n' +
-                `Status Code: ${statusCode}`);
-        } else if (!/^application\/json/.test(contentType)) {
-            error = new Error('Invalid content-type.\n' +
-                `Expected application/json but received ${contentType}`);
-        }
-        if (error) {
-            console.error(error.message);
-            // Consume response data to free up memory
-            res.resume();
-            return;
-        }
-
-        res.setEncoding('utf8');
-        let rawData = '';
-        res.on('data', (chunk) => { rawData += chunk; });
-        res.on('end', () => {
-            try {
-                const parsedData = JSON.parse(rawData);
-                return parsedData.isp;
-            } catch (e) {
-                console.error(e.message);
+            let error;
+            // Any 2xx status code signals a successful response but
+            // here we're only checking for 200.
+            if (statusCode !== 200) {
+                error = new Error('Request Failed.\n' +
+                    `Status Code: ${statusCode}`);
+            } else if (!/^application\/json/.test(contentType)) {
+                error = new Error('Invalid content-type.\n' +
+                    `Expected application/json but received ${contentType}`);
             }
+            if (error) {
+                console.error(error.message);
+                // Consume response data to free up memory
+                res.resume();
+                reject('something went wrong');
+            }
+
+            res.setEncoding('utf8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    resolve({ isp: parsedData.isp, ping: (endTime - startTime) / 10 });
+                } catch (e) {
+                    console.error(e.message);
+                    reject(e.message);
+                }
+            });
+        }).on('error', (e) => {
+            console.error(`Got error: ${e.message}`);
+            reject(e.message);
+
         });
-    }).on('error', (e) => {
-        console.error(`Got error: ${e.message}`);
     });
+
 }
+
+
+
+// getNetworkDownloadSpeed().then(function (params) {
+//     console.log('download');
+//     console.log(params);
+// });
+
+// getNetworkUploadSpeed().then(function (params) {
+//     console.log('upload');
+//     console.log(params);
+// });
+
+// getServiceProvider().then(function (params) {
+//     console.log(params.isp);
+//     console.log(params.ping);
+// });
+
 
 
