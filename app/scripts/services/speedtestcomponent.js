94a13bc5f36bb6af8d5bcb9a83a54c457eb53375
Cherrie Mae Andrea Sun <cesun@up.edu.ph>
2021-05-04 14:17:01 +0800

    Attempt 10

--- app/scripts/services/speedtestcomponent.js
+++ app/scripts/services/speedtestcomponent.js
@@ -7,7 +7,7 @@
  * # speedTestComponent
  * Service in the speedtestComponentApp.
  */
-var speedtestComponentApp = angular.module('speedtestComponentApp', ['jsBandwidth']);
+var speedtestComponentApp = angular.module('speedtestComponentApp');
 speedtestComponentApp.service('speedTestComponent', function ($window, $http, jsBandwidth) {
   // AngularJS will instantiate a singleton by calling "new" on this function
 
@@ -24,19 +24,19 @@ speedtestComponentApp.service('speedTestComponent', function ($window, $http, js
       }
     },
     getNetworkUploadSpeed: async function () {
-      // const testNetworkSpeed = new $window.NetworkSpeed();
-      // var options = {
-      //   hostname: 'www.google.com',
-      //   port: 80,
-      //   path: '/catchers/544b09b4599c1d0200000289',
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      // };
-      // var fileSizeInBytes = 4000000;
-      // var speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-      return jsBandwidth.testSpeed({});
+      const testNetworkSpeed = new $window.NetworkSpeed();
+      var options = {
+        hostname: 'http://httpbin.org/',
+        // port: 80,
+        // path: '/catchers/544b09b4599c1d0200000289',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+      var fileSizeInBytes = 4000000;
+      var speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+      return speed;
     },
     getServiceProvider: async function () {
       return new Promise(function (resolve, reject) {
