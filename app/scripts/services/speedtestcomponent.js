89a75e202e8463728950cf606dc8034ee0036dee
Cherrie Mae Andrea Sun <cesun@up.edu.ph>
2021-05-04 13:58:18 +0800

    Attempt 9

--- app/scripts/services/speedtestcomponent.js
+++ app/scripts/services/speedtestcomponent.js
@@ -7,14 +7,14 @@
  * # speedTestComponent
  * Service in the speedtestComponentApp.
  */
-var speedtestComponentApp = angular.module('speedtestComponentApp', []);
-speedtestComponentApp.service('speedTestComponent', function ($window, $http) {
+var speedtestComponentApp = angular.module('speedtestComponentApp', ['jsBandwidth']);
+speedtestComponentApp.service('speedTestComponent', function ($window, $http, jsBandwidth) {
   // AngularJS will instantiate a singleton by calling "new" on this function
 
   return {
     getNetworkDownloadSpeed: async function () {
       try {
-        const testNetworkSpeed = new $window.NetworkSpeedCheck();
+        const testNetworkSpeed = new $window.NetworkSpeed();
         var baseUrl = 'https://eu.httpbin.org/stream-bytes/6000000';
         var fileSizeInBytes = 6000000;
         var speed = await testNetworkSpeed.checkDownloadSpeed(baseUrl, fileSizeInBytes);
@@ -24,57 +24,27 @@ speedtestComponentApp.service('speedTestComponent', function ($window, $http) {
       }
     },
     getNetworkUploadSpeed: async function () {
-      const testNetworkSpeed = new $window.NetworkSpeedCheck();
-      var options = {
-        hostname: 'www.google.com',
-        port: 80,
-        path: '/catchers/544b09b4599c1d0200000289',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      };
-      var fileSizeInBytes = 4000000;
-      var speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
-      return speed;
+      // const testNetworkSpeed = new $window.NetworkSpeed();
+      // var options = {
+      //   hostname: 'www.google.com',
+      //   port: 80,
+      //   path: '/catchers/544b09b4599c1d0200000289',
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //   },
+      // };
+      // var fileSizeInBytes = 4000000;
+      // var speed = await testNetworkSpeed.checkUploadSpeed(options, fileSizeInBytes);
+      return jsBandwidth.testSpeed({});
     },
     getServiceProvider: async function () {
       return new Promise(function (resolve, reject) {
         var startTime = new Date().getTime();
         $http.get('http://ip-api.com/json/').then(function (res) {
-          const { statusCode } = res;
-          const contentType = res.headers['content-type'];
+          console.log(res);
           var endTime = new Date().getTime();
-
-          let error;
-          // Any 2xx status code signals a successful response but
-          // here we're only checking for 200.
-          if (statusCode !== 200) {
-            error = new Error('Request Failed.\n' +
-              `Status Code: ${statusCode}`);
-          } else if (!/^application\/json/.test(contentType)) {
-            error = new Error('Invalid content-type.\n' +
-              `Expected application/json but received ${contentType}`);
-          }
-          if (error) {
-            console.error(error.message);
-            // Consume response data to free up memory
-            res.resume();
-            reject('something went wrong');
-          }
-
-          res.setEncoding('utf8');
-          let rawData = '';
-          res.on('data', (chunk) => { rawData += chunk; });
-          res.on('end', () => {
-            try {
-              const parsedData = JSON.parse(rawData);
-              resolve({ isp: parsedData.isp, ping: (endTime - startTime) / 10 });
-            } catch (e) {
-              console.error(e.message);
-              reject(e.message);
-            }
-          });
+          resolve({ isp: res.data.isp, ping: (endTime - startTime) / 10 });
         }).catch(function (e) {
           console.error(`Got error: ${e.message}`);
           reject(e.message);
